62f5456d064f8a1a1ebf28a5b8a4e5e38859f820
Rucksolly <rucksolly.tech@gmail.com>
2023-11-10 21:08:28 +0100

    Our first commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <p>Hello my react APP</p>
+        <small>Starting a new project</small>
+    </div>
+  )
+}
+
+export default Home
+
+
+
